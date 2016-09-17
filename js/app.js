db204afc8d5800179d6b1861ca3f4b46db897ca8
rhondarobot <rrachellej@gmail.com>
2016-09-16 20:07:34 -0700

    fixed minor changes. configuring xml parsing

--- js/app.js
+++ js/app.js
@@ -28,10 +28,10 @@ $.ajax({
 	});
 })
 //switch from xml to json
-var xml = 'https://www.zillow.com/webservice/GetDeepSearchResults.htm',
+var xml = '<SearchResults:searchresults xsi:schemaLocation="http://www.zillow.com/static/xsd/SearchResults.xsd /vstatic/ae1bf8a790b67ef2e902d2bc04046f02/static/xsd/SearchResults.xsd">',
 $xmlDoc = $.parseXML(xml)
-$xml = $(xmldoc),
-$citystatezip = $xml.find("citystatezip");	
+$xml = $(xmlDoc),
+$citystatezip = $xml.find("searchresults");	
 
 
 };//end of code
