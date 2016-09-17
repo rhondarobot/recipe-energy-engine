0ec61fe14841c90d25d2ca4e73d5d8ae0d35608f
rhondarobot <rrachellej@gmail.com>
2016-09-16 20:18:50 -0700

    fixed minor changes. configuring xml parsing

--- js/app.js
+++ js/app.js
@@ -28,10 +28,10 @@ $.ajax({
 	});
 })
 //switch from xml to json
-var xml = '<SearchResults:searchresults xsi:schemaLocation="http://www.zillow.com/static/xsd/SearchResults.xsd /vstatic/ae1bf8a790b67ef2e902d2bc04046f02/static/xsd/SearchResults.xsd">',
+var xml = '<citystatezip>',
 $xmlDoc = $.parseXML(xml)
 $xml = $(xmlDoc),
-$citystatezip = $xml.find("searchresults");	
+$citystatezip = $xml.find("citystatezip");	
 
 
 };//end of code
