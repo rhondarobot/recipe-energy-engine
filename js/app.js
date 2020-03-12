f929c3a9bb7ecccfe89f4b0e7793b62f3a621f8b
rhondarobot <rrachellej@gmail.com>
2020-03-12 16:54:37 -0700

    fixes broken app

--- js/app.js
+++ js/app.js
@@ -113,9 +113,8 @@ var showRecipe = function(item) {
  function findRecipe(recipeSearch,low,high) {
     var recipeParams = {
         q: recipeSearch,
-        // 'app_key': 'X1-ZWz19kmj7cegwb_1acr8',
-        'app_key': '765395435942f67b7d9970497f622f75',
-        'app_id': '4bdd672f',
+        app_key: '3307e1133463374fba27540d8dd5ea7d',
+        app_id: '4bdd672f',
         calories: 'gte '+ low + ',lte ' + high,
         from: 0,
         to: 16
